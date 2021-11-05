8e2f3e1066d23bc8930daaf58d91e9c9d6cdf5dc
Sam Kelsey <sam_kelsey@hotmail.co.uk>
2021-11-05 22:56:27 +0000

    :loud_sound: Added basic logger to quicksorter class.

--- sorter-socket/src/main/java/com/samkelsey/sortersocket/service/sorter/QuickSorterImpl.java
+++ sorter-socket/src/main/java/com/samkelsey/sortersocket/service/sorter/QuickSorterImpl.java
@@ -1,5 +1,7 @@
 package com.samkelsey.sortersocket.service.sorter;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.stereotype.Service;
 
@@ -8,13 +10,15 @@ import java.util.List;
 @Service
 public class QuickSorterImpl extends Sorter {
 
+    Logger logger = LoggerFactory.getLogger(QuickSorterImpl.class);
+
     public QuickSorterImpl(SimpMessageSendingOperations simpMessageSendingOperations) {
         super(simpMessageSendingOperations, "Quicksort");
     }
 
     @Override
     public List<Integer> sort(List<Integer> unsortedList) throws InterruptedException {
-        System.out.println("Quick sorting...");
+        logger.info("Quick sorting...");
         return null;
     }
 }
