06141908661a67d91ffed3b34551d2a990b453df
Sam Kelsey <sam_kelsey@hotmail.co.uk>
2021-11-18 20:10:39 +0000

    :white_check_mark: Finished first socket integration test.

--- sorter-socket/src/test/java/com/samkelsey/sortersocket/controller/SorterControllerIntegrationTest.java
+++ sorter-socket/src/test/java/com/samkelsey/sortersocket/controller/SorterControllerIntegrationTest.java
@@ -1,6 +1,7 @@
 package com.samkelsey.sortersocket.controller;
 
 import com.samkelsey.sortersocket.TestUtils;
+import com.samkelsey.sortersocket.dto.model.SorterRequestDto;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.slf4j.Logger;
@@ -8,13 +9,21 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.messaging.converter.MappingJackson2MessageConverter;
+import org.springframework.messaging.simp.stomp.StompHeaders;
 import org.springframework.messaging.simp.stomp.StompSession;
-import org.springframework.messaging.simp.stomp.StompSessionHandler;
+import org.springframework.messaging.simp.stomp.StompSessionHandlerAdapter;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.web.socket.client.WebSocketClient;
 import org.springframework.web.socket.client.standard.StandardWebSocketClient;
 import org.springframework.web.socket.messaging.WebSocketStompClient;
 
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Stack;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 
 @SpringBootTest(webEnvironment = RANDOM_PORT)
@@ -27,28 +36,42 @@ public class SorterControllerIntegrationTest {
     Logger logger = LoggerFactory.getLogger(SorterControllerIntegrationTest.class);
 
     @Test
-    void shouldReturnOK_whenValidPayload() throws Exception {
+    void shouldReturnSortedList_whenValidPayload() throws Exception {
         WebSocketStompClient stompClient = getStompClient();
-        StompSessionHandler sessionHandler = new MyStompSessionHandler();
+        Stack<List<Integer>> receivedMessages = new Stack<>();
 
         StompSession session = stompClient.connect(
                 String.format("ws://localhost:%d/socket", port),
-                sessionHandler
+                new StompSessionHandlerAdapter() {
+                    @Override
+                    public Type getPayloadType(StompHeaders headers) {
+                        return SorterRequestDto.class;
+                    }
+
+                    @Override
+                    public void handleFrame(StompHeaders headers, Object payload) {
+                        SorterRequestDto msg = (SorterRequestDto) payload;
+                        receivedMessages.push(msg.getSortingList());
+                        logger.info(String.format("Sorting list status: %s", msg.getSortingList().toString()));
+                        super.handleFrame(headers, payload);
+                    }
+
+                    @Override
+                    public void afterConnected(StompSession session, StompHeaders connectedHeaders) {
+                        session.subscribe("/sorting", this);
+                        session.subscribe("/errors", this);
+                        logger.info("Client connected");
+                        super.afterConnected(session, connectedHeaders);
+                    }
+                }
         ).get();
 
-        Thread.sleep(1000);
         session.send("/app/sort", TestUtils.createSorterRequestDto());
-
-//        JSONObject json = new JSONObject();
-//        try {
-//            json.put("sorting-list", new int[]{1, 3, 2, 5});
-//        } catch (Exception e) {
-//            logger.error(e.getMessage());
-//        }
-
-//        Thread.sleep(1000);
-
-//        session.send("/app/sort", new int[]{1, 3, 2, 5});
+        // TODO: Find way to keep test running until connection is closed (all sorting messages have been sent)
+        Thread.sleep(2000);
+        List<Integer> expected = new ArrayList<>(Arrays.asList(1, 2, 3, 3, 8));
+        assertEquals(expected, receivedMessages.pop());
+        session.disconnect();
 
     }
 
