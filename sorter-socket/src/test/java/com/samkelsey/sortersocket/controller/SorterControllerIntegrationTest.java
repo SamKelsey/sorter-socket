2e8bbb1259a09bd81ce6af8ac1853ec7f1d38ad5
Sam Kelsey <sam_kelsey@hotmail.co.uk>
2021-11-25 20:06:47 +0000

    :white_check_mark: Refactored integration test class.

--- sorter-socket/src/test/java/com/samkelsey/sortersocket/controller/SorterControllerIntegrationTest.java
+++ sorter-socket/src/test/java/com/samkelsey/sortersocket/controller/SorterControllerIntegrationTest.java
@@ -6,6 +6,7 @@ import com.samkelsey.sortersocket.TestUtils;
 import com.samkelsey.sortersocket.dto.model.SorterResponseDto;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -13,6 +14,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.converter.MappingJackson2MessageConverter;
 import org.springframework.messaging.simp.stomp.StompCommand;
 import org.springframework.messaging.simp.stomp.StompHeaders;
@@ -28,24 +30,32 @@ import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.PriorityQueue;
+import java.util.Objects;
 import java.util.Stack;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 
+/**
+ * An integration test class that uses a websocket client to post requests to the sorter server endpoint.
+ */
 @SpringBootTest(webEnvironment = RANDOM_PORT)
 @ExtendWith(SpringExtension.class)
 public class SorterControllerIntegrationTest {
 
     @Value("${local.server.port}")
     private int port;
-    private WebSocketStompClient stompClient;
-    private final Stack<List<Integer>> receivedMessages = new Stack<>();
-    private PriorityQueue<String> receivedErrors = new PriorityQueue<>();
-    private String SOCKET_ENDPOINT;
 
-    Logger logger = LoggerFactory.getLogger(SorterControllerIntegrationTest.class);
+    private WebSocketStompClient stompClient;
+    private StompSession session;
+
+    private final Stack<List<Integer>> receivedMessages = new Stack<>();
+    private final Stack<ResponseEntity<String>> receivedErrors = new Stack<>();
+
+    private String SOCKET_ENDPOINT;
+    private final String SORTER_ENDPOINT = "/app/sort";
+
+    private final int THREAD_SLEEP_DURATION = 2000;
 
     @BeforeEach
     void init() {
@@ -61,7 +71,7 @@ public class SorterControllerIntegrationTest {
 
     @Test
     void shouldReturnSortedList_whenValidPayload() throws Exception {
-        StompSession session = stompClient.connect(
+        session = stompClient.connect(
                 SOCKET_ENDPOINT,
                 new MyStompSessionHandler() {
                     @Override
@@ -71,18 +81,16 @@ public class SorterControllerIntegrationTest {
                 }
         ).get();
 
-        session.send("/app/sort", TestUtils.createSorterRequestDto());
-        Thread.sleep(2000);
+        session.send(SORTER_ENDPOINT, TestUtils.createSorterRequestDto());
+        Thread.sleep(THREAD_SLEEP_DURATION);
 
         List<Integer> expected = new ArrayList<>(Arrays.asList(1, 2, 3, 3, 8));
         assertEquals(expected, receivedMessages.pop());
-
-        session.disconnect();
     }
 
     @Test
     void shouldReturnException_whenInvalidPayload() throws Exception {
-        StompSession session = stompClient.connect(
+        session = stompClient.connect(
                 SOCKET_ENDPOINT,
                 new MyStompSessionHandler() {
                     @Override
@@ -92,10 +100,21 @@ public class SorterControllerIntegrationTest {
                 }
         ).get();
 
-        session.send("/app/sort", TestUtils.createInvalidSorterRequestDto());
-        Thread.sleep(2000);
-        System.out.println(receivedErrors);
-        // TODO: Need to invoke assertion to check correct error message returned.
+        session.send(SORTER_ENDPOINT, TestUtils.createInvalidSorterRequestDto());
+        Thread.sleep(THREAD_SLEEP_DURATION);
+
+        ResponseEntity<String> result = receivedErrors.pop();
+        ResponseEntity<String> expected = ResponseEntity
+                .status(400)
+                .body("Bad request: sorting-list cannot be null.");
+
+        assertEquals(expected.getStatusCode(), result.getStatusCode());
+        assertEquals(expected.getBody(), result.getBody());
+    }
+
+    @AfterEach
+    void teardown() {
+        session.disconnect();
     }
 
     private abstract class MyStompSessionHandler extends StompSessionHandlerAdapter {
@@ -103,34 +122,7 @@ public class SorterControllerIntegrationTest {
         Logger logger = LoggerFactory.getLogger(MyStompSessionHandler.class);
 
         @Override
-        public void handleException(StompSession session, StompCommand command, StompHeaders headers, byte[] payload, Throwable exception) {
-            logger.error(String.format("Error occured: %s", exception));
-            super.handleException(session, command, headers, payload, exception);
-        }
-
-        @Override
         public abstract Type getPayloadType(StompHeaders headers);
-
-        @Override
-        public void handleFrame(StompHeaders headers, Object payload) {
-            try {
-                String destination = headers.getDestination();
-                Object dto = deserializePayload(destination, payload);
-
-                if (destination.equals("/sorting")) {
-                    receivedMessages.push(((SorterResponseDto) dto).getSortingList());
-                    logger.info(String.format("Sorting list status: %s", ((SorterResponseDto) dto).getSortingList()));
-                } else if (destination.equals("/errors")) {
-                    receivedErrors.add((String) dto);
-                    logger.info(String.format("Error received: %s", (String) dto));
-                }
-
-            } catch (Exception e) {
-                logger.error(e.getMessage());
-            }
-
-            super.handleFrame(headers, payload);
-        }
 
         @Override
         public void afterConnected(StompSession session, StompHeaders connectedHeaders) {
@@ -140,17 +132,54 @@ public class SorterControllerIntegrationTest {
             super.afterConnected(session, connectedHeaders);
         }
 
-        private Object deserializePayload(String destination, Object payload) throws JSONException, JsonProcessingException {
-            ObjectMapper mapper = new ObjectMapper();
-            JSONObject json = new JSONObject(new String((byte []) payload, StandardCharsets.UTF_8));
-            if (destination.equals("/sorting")) {
-                JSONObject body = json.getJSONObject("body");
-                return mapper.readValue(body.toString(), SorterResponseDto.class);
-            } else if (destination.equals("/errors")) {
-                return json.getString("body");
+        @Override
+        public void handleFrame(StompHeaders headers, Object payload) {
+            try {
+                String destination = headers.getDestination();
+                if (Objects.isNull(destination)) {
+                    throw new NullPointerException("No destination specified");
+                }
+
+                Object dto = deserializePayload(destination, payload);
+
+                if (destination.equals("/sorting")) {
+                    receivedMessages.push(((SorterResponseDto) dto).getSortingList());
+                    logger.info(String.format("Sorting list status: %s", ((SorterResponseDto) dto).getSortingList()));
+                } else if (destination.equals("/errors")) {
+                    receivedErrors.push((ResponseEntity<String>) dto);
+                    logger.info(String.format("Error received: %s", dto.toString()));
+                }
+            } catch (Exception e) {
+                logger.error(e.getMessage());
             }
 
-            return null;
+            super.handleFrame(headers, payload);
+        }
+
+        @Override
+        public void handleException(StompSession session, StompCommand command, StompHeaders headers, byte[] payload, Throwable exception) {
+            logger.error(String.format("Error occured: %s", exception));
+            super.handleException(session, command, headers, payload, exception);
+        }
+
+        private Object deserializePayload(String destination, Object payload) throws JSONException, JsonProcessingException {
+            ObjectMapper mapper = new ObjectMapper();
+
+            JSONObject json = new JSONObject(
+                    new String((byte []) payload,
+                    StandardCharsets.UTF_8));
+
+            switch (destination) {
+                case "/sorting":
+                    JSONObject body = json.getJSONObject("body");
+                    return mapper.readValue(body.toString(), SorterResponseDto.class);
+                case "/errors":
+                    return ResponseEntity
+                        .status(json.getInt("statusCodeValue"))
+                        .body(json.getString("body"));
+                default:
+                    return null;
+            }
         }
     }
 }
